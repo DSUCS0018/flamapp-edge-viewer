1eb1baadb5065abcfac5500f1fd17ccc7df7a838
Ankita Ishwar Bellad <ankitabellad88@gmail.com>
2025-11-14 14:39:23 +0530

    chore: android project structure – created folders + placeholder Kotlin/C++/manifest

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,7 @@
+﻿#include <jni.h>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_flamapp_edge_NativeBridge_placeholder(JNIEnv *env, jobject thiz) {
+    // Placeholder - real native code will be added later
+}
---
+++ app/src/main/java/com/flamapp/edge/MainActivity.kt
@@ -0,0 +1,5 @@
+﻿package com.flamapp.edge
+
+class MainActivity {
+    // Placeholder - implementation will be added in next tasks
+}
